dd62427dd1eda5fd0195f68f6b1e05b14854b2db
Mariví <marivirh82@gmail.com>
2018-07-20 12:47:39 +0200

    replace var for const and let and change functions to arrow functions

--- js/selectSkills.js
+++ js/selectSkills.js
@@ -21,7 +21,43 @@ fetch('https://raw.githubusercontent.com/Adalab/dorcas-s2-proyecto-data/master/s
 
     }
 
-    function actualizaTarjeta() {
+    const añadeSelect = (e) => {
+
+      const boton = e.currentTarget;
+      console.log('e', boton);
+      console.log(boton.firstChild.className);
+
+      if (boton.firstChild.className === 'fas fa-plus') {
+        if (contador < 3) {
+          contador++;
+          boton.innerHTML = `${minus}`;
+          creaSelect();
+          botones(btn);
+        }
+      } else {
+        boton.innerHTML = `${plus}`;
+        borraSelect();
+        botones(btn);
+      }
+    };
+
+    const botones = (btn) => {
+
+      btn = document.querySelectorAll('.btn-select');
+      // Hacer un map
+      for (let i = 0; i < btn.length; i++) {
+
+        btn[i].addEventListener('click', añadeSelect);
+
+      }
+      console.log(btn.length);
+
+      // btn.map(function(button) {
+      //   return button.addEventListener('click', añadeSelect);
+      // });
+    };
+
+    const actualizaTarjeta = () => {
 
       const listSelects = document.querySelectorAll('.skills--stuffed');
       const spans = document.querySelectorAll('.icon__text');
@@ -30,12 +66,11 @@ fetch('https://raw.githubusercontent.com/Adalab/dorcas-s2-proyecto-data/master/s
 
       for (let i = 0; i < listSelects.length; i++) {
         spans[i].innerHTML = '';
-        spans[i].innerHTML = listSelects[i].value;
+        spans[i].innerHTML = `${listSelects[i].value}`;
       }
+    };
 
-    }
-
-    function creaSelect() {
+    const creaSelect = () => {
 
       const newSelect = document.createElement('select');
       newSelect.classList.add('skills--stuffed');
@@ -65,50 +100,14 @@ fetch('https://raw.githubusercontent.com/Adalab/dorcas-s2-proyecto-data/master/s
       newSelect.addEventListener('change', actualizaTarjeta);
 
       return newSelect;
+    };
 
-    }
-
-    function borraSelect() {
+    const borraSelect = () => {
       contenedor = document.querySelector('.label--stuffed__container-select');
       contenedor.removeChild(contenedor.lastChild);
       contador--;
       //actualizaTarjeta();
-    }
-
-
-    function añadeSelect(e) {
-
-      const boton = e.currentTarget;
-      console.log('e', boton);
-      console.log(boton.firstChild.className);
-
-      if (boton.firstChild.className === 'fas fa-plus') {
-        if (contador < 3) {
-          contador++;
-          boton.innerHTML = minus;
-          creaSelect();
-          botones(btn);
-        }
-      } else {
-        boton.innerHTML = plus;
-        borraSelect();
-        botones(btn);
-      }
-
-
-    }
-
-    function botones(btn) {
-
-      btn = document.querySelectorAll('.btn-select');
-      for (let i = 0; i < btn.length; i++) {
-
-        btn[i].addEventListener('click', añadeSelect);
-
-      }
-      console.log(btn.length);
-    }
-
+    };
     creaSelect();
     botones();
   });
