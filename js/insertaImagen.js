63e59ff47eb80b81fdaf94d4ae2b3285ec99d393
Raquel Romo <Raquel@MacBook-Pro-de-Eduardo.local>
2018-07-20 11:15:25 +0200

    local and image updated ES6

--- js/insertaImagen.js
+++ js/insertaImagen.js
@@ -1,28 +1,38 @@
 'use strict';
 
 const fr = new FileReader();
-
 const btnImg = document.querySelector('.input--stuffed__img');
 const inputImg = document.querySelector('#img--profile');
 const profileImg = document.querySelector('.photo');
 const preview = document.querySelector('.preview');
 
+// function writeImage() {
+//   profileImg.src = fr.result;
+//   preview.src = fr.result;
+// }
 
-function getImage(e) {
+const writeImage = () => {
+  profileImg.src = fr.result;
+  preview.src = fr.result;
+};
+
+// function getImage(e) {
+//   const myFile = e.currentTarget.files[0];
+//   fr.addEventListener('load', writeImage);
+//   fr.readAsDataURL(myFile);
+// }
+
+const getImage = (e) => {
   const myFile = e.currentTarget.files[0];
   fr.addEventListener('load', writeImage);
   fr.readAsDataURL(myFile);
-}
+};
 
-function writeImage() {
-  profileImg.src = fr.result;
-  preview.src = fr.result;
-}
+// function inputClick() {
+//   inputImg.click();
+// }
 
-function inputClick() {
-  inputImg.click();
-}
-
+const inputClick = () => inputImg.click();
 
 inputImg.addEventListener('change', getImage);
 btnImg.addEventListener('click', inputClick);
